ee7fd2211fb35f44c4d884a7d98f3cc8239bcc4e
Gaurav237 <gktiwary237@gmail.com>
2024-05-19 04:09:25 +0530

    all category apis added

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -9,6 +9,58 @@ const createCategory = async (req, res) => {
     }
 }
 
+const getAllCategories = async (req, res) => {
+    try{
+        const category = await Category.find({})
+        res.status(200).json(category)
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
+}
+
+const getCategoryById = async (req, res) => {
+    try{
+        const { id } = req.params
+        const category = await Category.findById(id)
+        if(!category){
+            return res.status(404).json({message: 'Category Not Found'})
+        }
+        res.status(200).json(category)
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
+}
+
+const getCategoryByName = async (req, res) => {
+    try{
+        const { name } = req.params
+        const category = await Category.find({name: name})
+        if(!category){
+            return res.status(404).json({message: 'Category Not Found'})
+        }
+        res.status(200).json(category)
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
+}
+
+const editCategory = async (req, res) => {
+    try{
+        const { id } = req.params
+        const category = await Category.findByIdAndUpdate(id, req.body, {new: true})
+        if(!category){
+            return res.status(404).json({message: 'Category Not Found'})
+        }
+        res.status(200).json(category)
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
+}
+
 module.exports = {
     createCategory,
+    getAllCategories,
+    getCategoryById,
+    getCategoryByName,
+    editCategory
 }
--- routes/category.route.js
+++ routes/category.route.js
@@ -1,7 +1,15 @@
 const express = require('express')
-const { createCategory } = require('../controllers/category.controller')
+const { createCategory, getAllCategories, getCategoryById, getCategoryByName, editCategory } = require('../controllers/category.controller')
 const router = express.Router()
 
 router.post('/', createCategory)
+
+router.get('/', getAllCategories)
+
+router.get('/id/:id', getCategoryById)
+
+router.get('/name/:name', getCategoryByName)
+
+router.put('/:id', editCategory)
 
 module.exports = router
