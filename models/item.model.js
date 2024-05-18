fbfeb882cef8fed99fb9c35a695453bad9a1ff7d
Gaurav237 <gktiwary237@gmail.com>
2024-05-19 05:10:10 +0530

    Item Model created

---
+++ models/item.model.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const ItemSchema = new mongoose.Schema({
+    name: { 
+        type: String, 
+        required: true 
+    },
+    image: { 
+        type: String, 
+        required: true 
+    },
+    description: { 
+        type: String, 
+        required: true 
+    },
+    taxApplicability: { 
+        type: Boolean, 
+        required: true 
+    },
+    tax: { 
+        type: Number, 
+        default: 0 
+    },
+    baseAmount: { 
+        type: Number, 
+        required: true 
+    },
+    discount: { 
+        type: Number, 
+        default: 0 
+    },
+    totalAmount: { 
+        type: Number, 
+        required: true 
+    },
+    subCategory: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'SubCategory' 
+    },
+    category: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Category' 
+    }
+}, {
+    timestamps: true
+});
+
+const Item = mongoose.model('Item', ItemSchema)
+module.exports = Item
